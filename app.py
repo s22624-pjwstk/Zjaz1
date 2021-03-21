cc36378f7ee37af05f94ce33025125523fbb9dab
DarekKarasiewicz <dar.karasiewicz@gmail.com>
2021-03-21 11:56:35 +0100

    Zadanie 3

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template,request
+from flask import(
+    Flask, render_template,request,session,redirect,url_for)
 
+
+class User:
+    def __init__(self,id,username,password):
+        self.id=id
+        self.username= username
+        self.password= password
+    def __repr__(self):
+        return f'<User:{self.username}'
+
+users=[]
+users.append(User(id=1,username='Darek',password='password'))
+
+print(users)
 app = Flask(__name__)
+app.secret_key='sekretnyklucz'
+@app.route('/', methods=('GET','POST'))
+def login():
+    if request.method =='POST':
+        session.pop('user_id',None)
+        username=request.form['username']
+        password=request.form['password']
 
+        user=[x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id']=user.id
+            return redirect(url_for('calculator'))
 
-@app.route('/')
-def mian():
-    return render_template('index.html')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route("/calculator" ,methods=('GET','POST'))
+def calculator():
+    return render_template('calculator.html')
 
 @app.route('/example', methods=('GET','POST'))
 def example():
